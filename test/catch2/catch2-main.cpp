3bf7e71213962bd0afac8829ab56d88d84cc3e3d
Igor Krivenko <igor.s.krivenko@gmail.com>
2018-12-09 18:56:43 -0500

    [test] Speed up compilation by moving CATCH_CONFIG_MAIN into dedicated .cpp

---
+++ test/catch2/catch2-main.cpp
@@ -0,0 +1,15 @@
+/*******************************************************************************
+ *
+ * This file is part of ezARPACK, an easy-to-use C++ wrapper for
+ * the ARPACK-NG FORTRAN library.
+ *
+ * Copyright (C) 2016-2018 Igor Krivenko <igor.s.krivenko@gmail.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ ******************************************************************************/
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
