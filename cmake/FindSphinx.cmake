934693e6c49c7b1b8d84e3be888f24ba7ecac013
Igor Krivenko <igor.s.krivenko@gmail.com>
2019-10-28 21:53:56 -0400

    [cmake] Added CMake modules to detect Sphinx and Breathe

---
+++ cmake/FindSphinx.cmake
@@ -0,0 +1,14 @@
+#
+# This file is part of ezARPACK, an easy-to-use C++ wrapper for
+# the ARPACK-NG FORTRAN library.
+#
+# Copyright (C) 2016-2019 Igor Krivenko <igor.s.krivenko@gmail.com>
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+include(cmake/FindPythonModule.cmake)
+FindPythonModule(Sphinx sphinx)
+
+set(SPHINX_COMMAND "${PYTHON_EXECUTABLE}" "-m" "sphinx")
