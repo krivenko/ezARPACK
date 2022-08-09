f593f042ed590476371e137b117fd172fae08185
Igor Krivenko <igor.s.krivenko@gmail.com>
2022-08-09 17:15:17 +0200

    [test] Added MPI unit tests for `ublas` backend

---
+++ test/ublas/mpi/asymmetric.cpp
@@ -0,0 +1,264 @@
+/*******************************************************************************
+ *
+ * This file is part of ezARPACK, an easy-to-use C++ wrapper for
+ * the ARPACK-NG FORTRAN library.
+ *
+ * Copyright (C) 2016-2022 Igor Krivenko <igor.s.krivenko@gmail.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ ******************************************************************************/
+
+#include "common.hpp"
+
+//////////////////////////////////////////////
+// Eigenproblems with general real matrices //
+//////////////////////////////////////////////
+
+TEST_CASE("Asymmetric eigenproblem is solved", "[solver_asymmetric]") {
+
+  using solver_t = mpi::arpack_solver<ezarpack::Asymmetric, ublas_storage>;
+  using params_t = solver_t::params_t;
+
+  const int N = 100;
+  const double diag_coeff_shift = -0.55;
+  const double diag_coeff_amp = 1.0;
+  const int offdiag_offset = 3;
+  const double offdiag_coeff = -1.05;
+  const int nev = 10;
+
+  auto spectrum_parts = {
+      params_t::LargestMagnitude, params_t::SmallestMagnitude,
+      params_t::LargestReal,      params_t::SmallestReal,
+      params_t::LargestImag,      params_t::SmallestImag};
+
+  // Asymmetric matrix A
+  auto A = make_sparse_matrix<ezarpack::Asymmetric>(
+      N, diag_coeff_shift, diag_coeff_amp, offdiag_offset, offdiag_coeff);
+  // Inner product matrix
+  auto M = make_inner_prod_matrix<ezarpack::Asymmetric>(N);
+
+  auto set_init_residual_vector = [](solver_t& ar) {
+    int const block_start = ar.local_block_start();
+    int const block_size = ar.local_block_size();
+    for(int i = 0; i < block_size; ++i)
+      ar.residual_vector()[i] = double(i + block_start) / N;
+  };
+
+  // Matrix-distributed vector multiplication
+  auto mat_vec = mpi_mat_vec<false>(N, MPI_COMM_WORLD);
+
+  using vector_view_t = solver_t::vector_view_t;
+  using vector_const_view_t = solver_t::vector_const_view_t;
+
+  SECTION("Constructors") {
+    std::vector<std::vector<unsigned int>> block_sizes = {
+        {100}, {50, 50}, {34, 33, 33}, {25, 25, 25, 25}};
+    std::vector<std::vector<unsigned int>> block_starts = {
+        {0}, {0, 50}, {0, 34, 67}, {0, 25, 50, 75}};
+
+    auto comm_size = mpi::size(MPI_COMM_WORLD);
+    auto comm_rank = mpi::rank(MPI_COMM_WORLD);
+
+    if(comm_size <= 4) {
+      solver_t ar1(N, MPI_COMM_WORLD);
+      CHECK(ar1.local_block_size() == block_sizes[comm_size - 1][comm_rank]);
+      CHECK(ar1.local_block_start() == block_starts[comm_size - 1][comm_rank]);
+
+      solver_t ar2(block_sizes[comm_size - 1], MPI_COMM_WORLD);
+      CHECK(ar2.local_block_size() == block_sizes[comm_size - 1][comm_rank]);
+      CHECK(ar2.local_block_start() == block_starts[comm_size - 1][comm_rank]);
+    }
+  }
+
+  SECTION("Standard eigenproblem") {
+    auto Aop = [&](vector_const_view_t in, vector_view_t out) {
+      mat_vec(A, in, out);
+    };
+
+    solver_t ar(A.size1(), MPI_COMM_WORLD);
+    REQUIRE(ar.dim() == A.size1());
+
+    for(auto e : spectrum_parts) {
+      params_t params(nev, e, params_t::Ritz);
+      params.random_residual_vector = false;
+      set_init_residual_vector(ar);
+      ar(Aop, params);
+      CHECK(ar.nconv() >= nev);
+      check_eigenvectors(ar, A);
+      check_basis_vectors(ar);
+    }
+  }
+
+  SECTION("Generalized eigenproblem: invert mode") {
+    decltype(A) op_matrix = prod(inverse(M), A);
+
+    auto op = [&](vector_const_view_t in, vector_view_t out) {
+      mat_vec(op_matrix, in, out);
+    };
+    auto Bop = [&](vector_const_view_t in, vector_view_t out) {
+      mat_vec(M, in, out);
+    };
+
+    solver_t ar(A.size1(), MPI_COMM_WORLD);
+    REQUIRE(ar.dim() == A.size1());
+
+    for(auto e : spectrum_parts) {
+      params_t params(nev, e, params_t::Ritz);
+      params.random_residual_vector = false;
+      set_init_residual_vector(ar);
+      ar(op, Bop, solver_t::Inverse, params);
+      CHECK(ar.nconv() >= nev);
+      check_eigenvectors(ar, A, M);
+      check_basis_vectors(ar, M);
+    }
+  }
+
+  using cmat_type = matrix<dcomplex, column_major>;
+
+  SECTION("Generalized eigenproblem: Shift-and-Invert mode (real part)") {
+    dcomplex sigma(1.0, -0.1);
+    decltype(A) op_matrix = real(prod(inverse(A - sigma * cmat_type(M)), M));
+
+    auto op = [&](vector_const_view_t in, vector_view_t out) {
+      mat_vec(op_matrix, in, out);
+    };
+    auto Bop = [&](vector_const_view_t in, vector_view_t out) {
+      mat_vec(M, in, out);
+    };
+
+    solver_t ar(A.size1(), MPI_COMM_WORLD);
+    REQUIRE(ar.dim() == A.size1());
+
+    for(auto e : spectrum_parts) {
+      params_t params(nev, e, params_t::Ritz);
+      params.random_residual_vector = false;
+      params.sigma = sigma;
+      set_init_residual_vector(ar);
+      ar(op, Bop, solver_t::ShiftAndInvertReal, params);
+      CHECK(ar.nconv() >= nev);
+      check_eigenvectors_shift_and_invert(ar, A, M);
+      check_basis_vectors(ar, M);
+    }
+  }
+
+  SECTION("Generalized eigenproblem: Shift-and-Invert mode (imaginary part)") {
+    dcomplex sigma(1.0, -0.1);
+    decltype(A) op_matrix = imag(prod(inverse(A - sigma * cmat_type(M)), M));
+
+    auto op = [&](vector_const_view_t in, vector_view_t out) {
+      mat_vec(op_matrix, in, out);
+    };
+    auto Bop = [&](vector_const_view_t in, vector_view_t out) {
+      mat_vec(M, in, out);
+    };
+
+    solver_t ar(A.size1(), MPI_COMM_WORLD);
+    REQUIRE(ar.dim() == A.size1());
+
+    for(auto e : spectrum_parts) {
+      params_t params(nev, e, params_t::Ritz);
+      params.random_residual_vector = false;
+      params.sigma = sigma;
+      set_init_residual_vector(ar);
+      ar(op, Bop, solver_t::ShiftAndInvertImag, params);
+      CHECK(ar.nconv() >= nev);
+      check_eigenvectors_shift_and_invert(ar, A, M);
+      check_basis_vectors(ar, M);
+    }
+  }
+
+  SECTION("Indirect access to workspace vectors") {
+    solver_t ar(A.size1(), MPI_COMM_WORLD);
+    REQUIRE(ar.dim() == A.size1());
+
+    auto Aop = [&](vector_const_view_t, vector_view_t) {
+      auto in = ar.workspace_vector(ar.in_vector_n());
+      auto out = ar.workspace_vector(ar.out_vector_n());
+      mat_vec(A, in, out);
+    };
+
+    for(auto e : spectrum_parts) {
+      params_t params(nev, e, params_t::Ritz);
+      params.random_residual_vector = false;
+      set_init_residual_vector(ar);
+      ar(Aop, params);
+      CHECK(ar.nconv() >= nev);
+      check_eigenvectors(ar, A);
+      check_basis_vectors(ar);
+    }
+
+    CHECK_THROWS(ar.workspace_vector(-1));
+    CHECK_THROWS(ar.workspace_vector(3));
+  }
+
+  SECTION("Custom implementation of the Exact Shift Strategy") {
+    std::vector<int> p;
+    p.reserve(A.size1());
+    auto shifts_f = [&](solver_t::real_vector_const_view_t ritz_values_re,
+                        solver_t::real_vector_const_view_t ritz_values_im,
+                        solver_t::real_vector_const_view_t ritz_bounds,
+                        solver_t::real_vector_view_t shifts_re,
+                        solver_t::real_vector_view_t shifts_im) {
+      int np = shifts_re.size();
+      if(np == 0) return;
+
+      p.resize(np);
+      std::iota(p.begin(), p.end(), 0);
+      // p will contain the permutation that puts ritz_bounds in
+      // the descending order of magnitude
+      std::sort(p.begin(), p.end(),
+                [&](int i, int j) { return ritz_bounds(i) > ritz_bounds(j); });
+      // Apply permutation p to ritz_values_* and use the result to fill shifts
+      for(int i = 0; i < np; ++i) {
+        shifts_re(i) = ritz_values_re(p[i]);
+        shifts_im(i) = ritz_values_im(p[i]);
+      }
+    };
+
+    SECTION("Standard eigenproblem") {
+      auto Aop = [&](vector_const_view_t in, vector_view_t out) {
+        mat_vec(A, in, out);
+      };
+
+      solver_t ar(A.size1(), MPI_COMM_WORLD);
+      REQUIRE(ar.dim() == A.size1());
+
+      params_t params(nev, params_t::LargestMagnitude, params_t::Ritz);
+      params.random_residual_vector = false;
+      params.tolerance = 1e-10;
+      set_init_residual_vector(ar);
+      ar(Aop, params, shifts_f);
+      CHECK(ar.nconv() >= nev);
+      check_eigenvectors(ar, A);
+      check_basis_vectors(ar);
+    }
+
+    SECTION("Generalized eigenproblem: Shift-and-Invert mode (real part)") {
+      dcomplex sigma(1.0, -0.1);
+      decltype(A) op_matrix = real(prod(inverse(A - sigma * cmat_type(M)), M));
+
+      auto op = [&](vector_const_view_t in, vector_view_t out) {
+        mat_vec(op_matrix, in, out);
+      };
+      auto Bop = [&](vector_const_view_t in, vector_view_t out) {
+        mat_vec(M, in, out);
+      };
+
+      solver_t ar(A.size1(), MPI_COMM_WORLD);
+      REQUIRE(ar.dim() == A.size1());
+
+      params_t params(nev, params_t::LargestMagnitude, params_t::Ritz);
+      params.random_residual_vector = false;
+      params.tolerance = 1e-10;
+      params.sigma = sigma;
+      set_init_residual_vector(ar);
+      ar(op, Bop, solver_t::ShiftAndInvertReal, params, shifts_f);
+      CHECK(ar.nconv() >= nev);
+      check_eigenvectors_shift_and_invert(ar, A, M);
+      check_basis_vectors(ar, M);
+    }
+  }
+}
---
+++ test/ublas/mpi/common.hpp
@@ -0,0 +1,193 @@
+/*******************************************************************************
+ *
+ * This file is part of ezARPACK, an easy-to-use C++ wrapper for
+ * the ARPACK-NG FORTRAN library.
+ *
+ * Copyright (C) 2016-2022 Igor Krivenko <igor.s.krivenko@gmail.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ ******************************************************************************/
+#pragma once
+
+#include "ezarpack/mpi/arpack_solver.hpp"
+
+#include "../../mpi_util.hpp"
+#include "../common.hpp"
+
+// Compute matrix-vector product u = M v, where blocks of vectors v and u are
+// distributed among MPI ranks.
+template<bool Complex> class mpi_mat_vec : public mpi_dist_op_base {
+  using scalar_t = typename std::conditional<Complex, dcomplex, double>::type;
+
+  // M multiplied by a block of v
+  mutable vector<scalar_t> M_v_block;
+
+public:
+  mpi_mat_vec(int N, MPI_Comm const& comm)
+      : mpi_dist_op_base(N, comm), M_v_block(N) {}
+
+  template<typename MT, typename VT, typename REST>
+  void operator()(MT const& M, VT const& v, REST& res) const {
+    M_v_block = prod(
+        project(M, range(0, M.size1()),
+                range(local_block_start, local_block_start + local_block_size)),
+        v);
+
+    MPI_Datatype datatype = Complex ? MPI_CXX_DOUBLE_COMPLEX : MPI_DOUBLE;
+
+    MPI_Reduce_scatter(&M_v_block(0), &res(0), block_sizes.data(), datatype,
+                       MPI_SUM, comm);
+  }
+};
+
+// Compute dot product dot(v1, v2), where blocks of vectors v1 and v2 are
+// distributed among MPI ranks.
+template<bool Complex> class mpi_dot {
+  using scalar_t = typename std::conditional<Complex, dcomplex, double>::type;
+
+  MPI_Comm comm;
+
+public:
+  mpi_dot(MPI_Comm const& comm) : comm(comm) {}
+
+  template<typename VT1, typename VT2>
+  scalar_t operator()(VT1 const& v1, VT2 const& v2) const {
+    scalar_t partial_dot_prod = inner_prod(conj(v1), v2);
+
+    MPI_Datatype datatype = Complex ? MPI_CXX_DOUBLE_COMPLEX : MPI_DOUBLE;
+    scalar_t dot_prod;
+    MPI_Allreduce(&partial_dot_prod, &dot_prod, 1, datatype, MPI_SUM, comm);
+
+    return dot_prod;
+  }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+// Check that 'ar' contains the correct solution of a standard eigenproblem
+template<operator_kind MKind, typename M>
+void check_eigenvectors(mpi::arpack_solver<MKind, ublas_storage> const& ar,
+                        M const& A) {
+  auto lambda = ar.eigenvalues();
+  auto vecs = ar.eigenvectors();
+
+  constexpr bool const ComplexEigenVecs = (MKind != ezarpack::Symmetric);
+  mpi_mat_vec<ComplexEigenVecs> prod(ar.dim(), MPI_COMM_WORLD);
+
+  vector<typename decltype(vecs)::value_type> lhs(ar.local_block_size());
+  for(int i = 0; i < lambda.size(); ++i) {
+    auto vec = column(vecs, i);
+    prod(A, vec, lhs);
+    CHECK_THAT(lhs, IsCloseTo(lambda(i) * vec));
+  }
+}
+
+// Check that 'ar' contains the correct solution of a generalized eigenproblem
+template<operator_kind MKind, typename MT>
+void check_eigenvectors(mpi::arpack_solver<MKind, ublas_storage> const& ar,
+                        MT const& A,
+                        MT const& M) {
+  auto lambda = ar.eigenvalues();
+  auto vecs = ar.eigenvectors();
+
+  constexpr bool const ComplexEigenVecs = (MKind != ezarpack::Symmetric);
+  mpi_mat_vec<ComplexEigenVecs> prod(ar.dim(), MPI_COMM_WORLD);
+
+  vector<typename decltype(vecs)::value_type> lhs(ar.local_block_size());
+  vector<typename decltype(vecs)::value_type> rhs(ar.local_block_size());
+  for(int i = 0; i < lambda.size(); ++i) {
+    auto vec = column(vecs, i);
+    prod(A, vec, lhs);
+    prod(M, vec, rhs);
+    rhs *= lambda(i);
+    CHECK_THAT(lhs, IsCloseTo(rhs));
+  }
+}
+
+// Check that 'ar' contains the correct solution of a generalized eigenproblem
+// (Asymmetric Shift-and-Invert modes)
+template<typename MT>
+void check_eigenvectors_shift_and_invert(
+    mpi::arpack_solver<ezarpack::Asymmetric, ublas_storage> const& ar,
+    MT const& A,
+    MT const& M) {
+  using solver_t = arpack_solver<ezarpack::Asymmetric, ublas_storage>;
+  using vector_view_t = solver_t::vector_view_t;
+  using vector_const_view_t = solver_t::vector_const_view_t;
+
+  mpi_mat_vec<false> prod(ar.dim(), MPI_COMM_WORLD);
+
+  auto Aop = [&](vector_const_view_t in, vector_view_t out) {
+    prod(A, in, out);
+  };
+  auto lambda = ar.eigenvalues(Aop);
+  auto vecs = ar.eigenvectors();
+
+  mpi_mat_vec<true> prod_complex(ar.dim(), MPI_COMM_WORLD);
+
+  vector<dcomplex> lhs(ar.local_block_size());
+  vector<dcomplex> rhs(ar.local_block_size());
+  for(int i = 0; i < lambda.size(); ++i) {
+    auto vec = column(vecs, i);
+    prod_complex(A, vec, lhs);
+    prod_complex(M, vec, rhs);
+    rhs *= lambda(i);
+    CHECK_THAT(lhs, IsCloseTo(rhs, 1e-9));
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+// In the real symmetric case, eigenvectors form an orthonormal basis
+auto get_basis_vectors(
+    mpi::arpack_solver<ezarpack::Symmetric, ublas_storage> const& ar)
+    -> decltype(ar.eigenvectors()) {
+  return ar.eigenvectors();
+}
+// In the other two cases we must call schur_vectors()
+template<operator_kind MKind>
+auto get_basis_vectors(mpi::arpack_solver<MKind, ublas_storage> const& ar)
+    -> decltype(ar.schur_vectors()) {
+  return ar.schur_vectors();
+}
+
+// Check orthogonality of basis vectors (standard eigenproblem)
+template<operator_kind MKind>
+void check_basis_vectors(mpi::arpack_solver<MKind, ublas_storage> const& ar) {
+  auto vecs = get_basis_vectors(ar);
+
+  constexpr bool const ComplexBasisVecs = MKind == ezarpack::Complex;
+  mpi_dot<ComplexBasisVecs> dot(MPI_COMM_WORLD);
+
+  for(int i = 0; i < vecs.size2(); ++i) {
+    auto vi = column(vecs, i);
+    for(int j = 0; j < vecs.size2(); ++j) {
+      auto vj = column(vecs, j);
+      CHECK(std::abs(dot(vi, vj) - double(i == j)) < 1e-10);
+    }
+  }
+}
+
+// Check orthogonality of basis vectors (generalized eigenproblem)
+template<operator_kind MKind, typename MT>
+void check_basis_vectors(mpi::arpack_solver<MKind, ublas_storage> const& ar,
+                         MT const& B) {
+  auto vecs = get_basis_vectors(ar);
+
+  constexpr bool const ComplexBasisVecs = MKind == ezarpack::Complex;
+  mpi_mat_vec<ComplexBasisVecs> prod(ar.dim(), MPI_COMM_WORLD);
+  mpi_dot<ComplexBasisVecs> dot(MPI_COMM_WORLD);
+
+  vector<typename decltype(vecs)::value_type> Bvj(ar.local_block_size());
+  for(int i = 0; i < vecs.size2(); ++i) {
+    auto vi = column(vecs, i);
+    for(int j = 0; j < vecs.size2(); ++j) {
+      auto vj = column(vecs, j);
+      prod(B, vj, Bvj);
+      CHECK(std::abs(dot(vi, Bvj) - double(i == j)) < 1e-10);
+    }
+  }
+}
